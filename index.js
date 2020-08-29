f5c4ff25655bbcfde78afe9ae0bdc5cb4480f22c
y0092888 <ju-horn@web.de>
2020-08-29 21:53:51 +0200

    update backend

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const { GraphQLServer } = require("graphql-yoga");
 
 const { resolvers } = require("./server/resolvers");
 
+const { PrismaClient } = require("@prisma/client");
+
+//npx prisma studio --experimental
+// to see the content
+const prisma = new PrismaClient();
+
 const server = new GraphQLServer({
   typeDefs: "./server/schema.graphql",
   resolvers,
+  context: { prisma },
 });
 
 const options = {
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,22 +1,19 @@
-let messages = [{ author: "a", id: 1, timeStamp: 0, text: "0" }];
-
-let id = 2;
+const { prismaVersion } = require("@prisma/client");
 
 const resolvers = {
   Query: {
     hello: () => "Hello World!",
-    feed: () => messages,
+    feed: async (p, args, context) => await context.prisma.message.findMany(),
   },
   Mutation: {
-    addMessage: (p, args) => {
-      const new_message = {
-        id: id++,
-        author: args.author,
-        text: args.text,
-        timeStamp: args.timeStamp,
-      };
-      messages = [new_message, ...messages];
-      return new_message;
+    addMessage: (p, args, context) => {
+      const message = context.prisma.message.create({
+        data: {
+          author: args.author,
+          text: args.text,
+        },
+      });
+      return message;
     },
   },
 };
